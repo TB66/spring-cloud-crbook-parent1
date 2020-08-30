c060820071ef700626738bc5504836365b71cf6b
TB66 <1132315537@qq.com>
2020-08-30 13:31:31 +0800

    crbook初始化 1.0

---
+++ spring-cloud-crbook-parent2/spring-cloud-crbook-user/src/test/java/com/tb/crbook/SpringCloudCrbookParent1ApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.tb.springcloudcrbookparent1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCrbookParent1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
